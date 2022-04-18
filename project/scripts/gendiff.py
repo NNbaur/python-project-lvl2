ccd80c505eb57294c1cd920dff987381e6cbee55
NNbaur <studentsoftpu@gmail.com>
2022-04-18 19:55:00 +0300

    second step of the second project

--- project/scripts/gendiff.py
+++ project/scripts/gendiff.py
@@ -4,6 +4,7 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('integers', metavar='first_file')
     parser.add_argument('integers', metavar='second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
 
     args = parser.parse_args()
     print(args.accumulate(args.integers))
