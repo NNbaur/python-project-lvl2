3ef724c9489c4b0b3b36be1494af6465e0f028e5
BaurNN <studentsoftpu@gmail.com>
2022-05-21 20:57:50 +0300

    linter corrections

--- project/formatter.py
+++ project/formatter.py
@@ -1,11 +1,10 @@
 def stylish(dict1, depth=2):
-    result = ''
+    res = ''
     lst = list(map(lambda x: x, dict1))
     lst.sort(key=lambda x: x[2:])
     for i in lst:
         if type(dict1[i]) is dict:
-            result += '\n' + depth * " " + f'{i}:'
-            f' {stylish(dict1[i], depth + 4)}'
+            res += '\n' + depth * " " + f'{i}: {stylish(dict1[i], depth + 4)}'
         else:
-            result += '\n' + depth * " " + f'{i}: {str(dict1[i])}'
-    return '{' + result + '\n' + (depth - 2) * " " + '}'
+            res += '\n' + depth * " " + f'{i}: {str(dict1[i])}'
+    return '{' + res + '\n' + (depth - 2) * " " + '}'
