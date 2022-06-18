5023d0859d0c0dbba6262604bd68ed42550b57e0
BaurNN <studentsoftpu@gmail.com>
2022-06-18 11:28:18 +0300

    delete testing file

--- testsss.py
+++
@@ -1,5 +0,0 @@
-from gendiff.diff_builder.generate_diff import generate_diff
-from gendiff.diff_builder.parse import parse_data, restructuring_path
-
-a = generate_diff('tests/fixtures/filepath3.yml', 'tests/fixtures/filepath4.yml', 'plain')
-print(a)
